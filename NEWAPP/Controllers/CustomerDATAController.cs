efcc51b64baa2d98c58f87be76e23790f6a9b92c
mediga vamshi <mediga-vamshi@priyanet.com>
2024-09-09 17:54:26 +0530

    Toaster

--- NEWAPP/Controllers/CustomerDATAController.cs
+++ NEWAPP/Controllers/CustomerDATAController.cs
@@ -14,7 +14,7 @@ namespace NEWAPP.Controllers
             _customerDATARepository= customerDATARepository;
         }
         public  async Task<ActionResult> Index()
-        {
+       {
             List<CustomerData> CustomerData = await _customerDATARepository.GetAllAsync();
             return View(CustomerData);
         }
@@ -40,54 +40,80 @@ namespace NEWAPP.Controllers
             try
             {
                 int id = await _customerDATARepository.InsertAsync(customerData);
+                TempData["Message"] = "Customer data created successfully!";
+                TempData["MessageType"] = "success"; // 'success', 'error', 'info', 'warning'
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
+                TempData["Message"] = "Error occurred while creating customer data.";
+                TempData["MessageType"] = "error";
                 return View();
             }
         }
 
         // GET: CustomerDATAController/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<ActionResult> Edit(int id)
         {
-            return View();
+            CustomerData customerData= await _customerDATARepository.GetByIdAsync(id);
+            return View(customerData);
         }
 
         // POST: CustomerDATAController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<ActionResult> Edit(CustomerData customerData)
         {
             try
             {
+                int id = await _customerDATARepository.UpdateAsync(customerData);
+                TempData["Message"] = "Customer data updated successfully!";
+                TempData["MessageType"] = "success";
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                TempData["Message"] = "Error occurred while updating customer data.";
+                TempData["MessageType"] = "error";
+                return View(customerData);
             }
         }
 
         // GET: CustomerDATAController/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
-            return View();
+            CustomerData customerData = await _customerDATARepository.GetByIdAsync(id);
+            return View(customerData);
         }
 
         // POST: CustomerDATAController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<ActionResult> Delete(int id, IFormCollection collection)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                bool result = await _customerDATARepository.DeleteAsync(id);
+                if (result)
+                {
+                    TempData["Message"] = "Customer data deleted successfully!";
+                    TempData["MessageType"] = "success";
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    TempData["Message"] = "Error occurred while deleting customer data.";
+                    TempData["MessageType"] = "error";
+                    return RedirectToAction(nameof(Index));
+                }
             }
             catch
             {
-                return View();
+                TempData["Message"] = "Error occurred while deleting customer data.";
+                TempData["MessageType"] = "error";
+                return View("Error");
             }
         }
+        
     }
 }
