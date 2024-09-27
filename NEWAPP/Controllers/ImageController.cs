8c20eaf8fab47fb44a3c2885772d3d24eca2e203
mediga vamshi <mediga-vamshi@priyanet.com>
2024-09-27 19:03:40 +0530

    base64

---
+++ NEWAPP/Controllers/ImageController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NEWAPP.Models;
+
+namespace NEWAPP.Controllers
+{
+    public class ImageController : Controller
+    {
+        // GET: ImageController
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ImageController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpGet]
+        public IActionResult ImageUpload()
+        {
+            return View(new ImageUploadViewModel());
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(ImageUploadViewModel model)
+        {
+            if (model.ImageFile != null && model.ImageFile.Length > 0)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    await model.ImageFile.CopyToAsync(memoryStream);
+                    var imageBytes = memoryStream.ToArray();
+                    model.Base64Image = Convert.ToBase64String(imageBytes);
+                }
+            }
+
+            return View("ImageUpload", model);
+        }
+
+
+
+    }
+}
---
+++ NEWAPP/Models/ImageUploadViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace NEWAPP.Models
+{
+    public class ImageUploadViewModel
+    {
+        public IFormFile ImageFile { get; set; }
+        public string Base64Image { get; set; }
+    }
+}
